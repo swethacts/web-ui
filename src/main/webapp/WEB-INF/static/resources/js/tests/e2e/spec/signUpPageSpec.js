94af663103519bc0f8134a3997ec8fe320ae6d3d
JATIN JOGI <jatin.jogi@cognizant.com>
2018-01-13 22:19:35 -0500

    update to protractor conf js

--- src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpPageSpec.js
+++ src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpPageSpec.js
@@ -6,6 +6,10 @@ describe('sqhub login page', function() {
 	   
    });
    
+   it('should load the home page', function() {		  
+	      expect(browser.getTitle()).toEqual('The Home Depot');
+   });
+   
    it('should sign in successfully with email and password', function() {
 		  application.signInUser();
 	      expect(browser.getTitle()).toEqual('The Home Depot');

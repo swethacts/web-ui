416d9e0870c56bac2af6849a628c0848dcff13bd
JATIN JOGI <jatin.jogi@cognizant.com>
2018-01-18 11:47:43 -0500

    changing conf js

--- html-source/protractor.conf.js
+++ html-source/protractor.conf.js
@@ -2,28 +2,27 @@ exports.config = {
   
     //seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar',
     //chromeDriver: 'node_modules/protractor/selenium/chromedriver',  IEDriverServer3.4.0.zip		
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
   
     //Location of E2E test specs
     specs: [
-     //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpHealthFirstPageSpec.js'
-     //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpPageSpec.js'
-	 //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHealthFirstPageSpec.js'	 
-    '../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHFOrgSpec.js'	 
+     '../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHFOrgSpec.js'
+    //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHealthFirstPageSpec.js'	 
+    //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpPageSpec.js'      
     ],
 	
 	//Communicates directly Chrome Driver or Firefox Driver, bypassing any Selenium Server. If this is true, settings for seleniumAddress and seleniumServerJar will be ignored
 	//directConnect: true,
 	
 	//Sauce Labs - While using sauce, comment the seleniumAddress
-  	sauceUser: 'connectns99',
-    sauceKey: 'a95eb41e-5c8e-4b9a-b64f-fae65d621834',
+  	//sauceUser: 'connectns99',
+    //sauceKey: 'a95eb41e-5c8e-4b9a-b64f-fae65d621834',
 
     // Capabilities to be passed to the webdriver instance.
   capabilities: {
     browserName: 'chrome',
-    version: 'latest',
-	platform: 'macOS 10.13',
+    //version: 'latest',
+	//platform: 'Windows 7',
 	//'time-zone': 'Detroit',
 	// maxSessions: 2,
 	//shardTestFiles: true,
@@ -31,8 +30,9 @@ exports.config = {
 	
 	chromeOptions: {
 		//args: ['no-sandbox', '--start-fullscreen']
-		//args: ['no-sandbox','headless']
-		args: ['no-sandbox']
+		args: ['no-sandbox','headless']
+		
+
 	}
 	
 	//proxy: {
--- html-source/protractor.conf.saucelabs.js
+++ html-source/protractor.conf.saucelabs.js
@@ -2,27 +2,28 @@ exports.config = {
   
     //seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar',
     //chromeDriver: 'node_modules/protractor/selenium/chromedriver',  IEDriverServer3.4.0.zip		
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
   
     //Location of E2E test specs
     specs: [
-     '../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHFOrgSpec.js'
-    //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHealthFirstPageSpec.js'	 
-    //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpPageSpec.js'      
+     //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpHealthFirstPageSpec.js'
+     //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/signUpPageSpec.js'
+	 //'../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHealthFirstPageSpec.js'	 
+    '../src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smokeHFOrgSpec.js'	 
     ],
 	
 	//Communicates directly Chrome Driver or Firefox Driver, bypassing any Selenium Server. If this is true, settings for seleniumAddress and seleniumServerJar will be ignored
 	//directConnect: true,
 	
 	//Sauce Labs - While using sauce, comment the seleniumAddress
-  	//sauceUser: 'connectns99',
-    //sauceKey: 'a95eb41e-5c8e-4b9a-b64f-fae65d621834',
+  	sauceUser: 'connectns99',
+    sauceKey: 'a95eb41e-5c8e-4b9a-b64f-fae65d621834',
 
     // Capabilities to be passed to the webdriver instance.
   capabilities: {
     browserName: 'chrome',
-    //version: 'latest',
-	//platform: 'Windows 7',
+    version: 'latest',
+	platform: 'macOS 10.13',
 	//'time-zone': 'Detroit',
 	// maxSessions: 2,
 	//shardTestFiles: true,
@@ -30,9 +31,8 @@ exports.config = {
 	
 	chromeOptions: {
 		//args: ['no-sandbox', '--start-fullscreen']
-		args: ['no-sandbox','headless']
-		
-
+		//args: ['no-sandbox','headless']
+		args: ['no-sandbox']
 	}
 	
 	//proxy: {

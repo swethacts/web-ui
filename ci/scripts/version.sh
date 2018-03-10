a655091df118953630fb6ad06c79779b060da120
Jogi, Jatin <>
2018-03-10 00:53:53 -0500

    version files

---
+++ ci/scripts/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -x
+
+#FETCH_VERSION=$(cat ../vr/"$VERSION_FILE")
+
+#if [[ $FETCH_VERSION == v* ]]; then 
+#	BUMPED_VER=$FETCH_VERSION
+#else
+#	BUMPED_VER=v$FETCH_VERSION
+#fi
+
+sleep 10
+
+echo "Artifact is now versioned"
+

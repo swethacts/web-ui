6f3d871824bbddcd6c104752f9681b7980c82eb8
Jogi, Jatin <>
2018-03-13 00:13:04 -0400

    threshold

--- ci/scripts/functional-test.sh
+++ ci/scripts/functional-test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #set -x
-
+PASSED_TESTS=7
+TOTAL_TESTS=7
 export DISPLAY=:99
 #chown cognizant:cognizant /etc/init.d/
 bash /etc/init.d/xvfb start
@@ -48,3 +49,16 @@ ls -ltr
 #	echo "Exiting with exit code 1..."
 #	exit 1
 #fi
+
+THRESHOLD=$SMOKE_TEST_THRESHOLD
+PASSED_TESTS_PERCENT=$((PASSED_TESTS*100))
+PASS_RATE=$(( PASSED_TESTS_PERCENT / TOTAL_TESTS ))
+echo "Pass Rate is "$PASS_RATE
+
+if [ "$PASS_RATE" -ge "$THRESHOLD" ]
+then
+	echo "Pass Rate is greater than or equal to Threshold set. ci-cd pipeline will continue to execute further"	
+else
+	echo "Pass Rate is lower than Threshold, this build will fail now. ci-cd pipeline will not continue to execute further"
+	exit 1
+fi

2b4c2ea2dd287c60366b232ebaf917760288cb3b
Jogi, Jatin <jatin_jogi@homedepot.com>
2018-04-14 16:53:10 -0400

    name change

--- src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smoke-spec-Tmobile.js
+++ src/main/webapp/WEB-INF/static/resources/js/tests/e2e/spec/smoke-spec-Tmobile.js
@@ -1,6 +1,6 @@
 var Application = require('./page/page-smoke-Tmobile.js');
 
-describe('NBCUniversal application', function() {
+describe('T-MOBILE.COM application', function() {
 	var application = new Application();
    beforeEach(function() {
 
